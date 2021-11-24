0ea4844f2303cbe22714d804bae017c9ecb81ecc
Nicolas Ott <nicolasott@gmail.com>
2021-11-24 19:17:03 +0100

    new gravity and block fixed

--- noGamX/ContentView.swift
+++ noGamX/ContentView.swift
@@ -17,63 +17,68 @@ class GameScene: SKScene {
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard let touch = touches.first else { return }
         let location = touch.location(in: self)
-        
-        let randomInt = Int.random(in: 1..<5)
+        let randomInt = Int.random(in: 1..<6)
         var boxColor = SKColor.red
         var boxWidth = 50
         var boxHeight = 50
         switch randomInt {
-        case 1:
-            boxColor = SKColor.green
-            boxWidth = 40
-            boxHeight = 40
-            physicsWorld.gravity = CGVector(dx: 0.0, dy: -4.8)
-            let box = SKSpriteNode(color: boxColor, size: CGSize(width: boxWidth, height: boxHeight))
-            box.position = location
-            box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: boxWidth, height: boxHeight))
-            addChild(box)
-            
-        case 2:
-            boxColor = SKColor.blue
-            boxWidth = 60
-            boxHeight = 60
-            physicsWorld.gravity = CGVector(dx: 0.0, dy: 0.5)
-            let texture1 = SKTexture(imageNamed: "a")
-            let arrow = SKSpriteNode(texture: texture1, color: UIColor.purple, size: CGSize(width: boxWidth, height: boxHeight))
-            arrow.position = location
-            arrow.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: boxWidth, height: boxHeight))
-            addChild(arrow)
-            
-        case 3:
-            boxColor = SKColor.yellow
-            boxWidth = 30
-            boxHeight = 30
-            let box = SKSpriteNode(color: boxColor, size: CGSize(width: boxWidth, height: boxHeight))
-            box.position = location
-            box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: boxWidth, height: boxHeight))
-            addChild(box)
-            
-        case 4:
-            boxColor = SKColor.red
-            boxWidth = 44
-            boxHeight = 4
-            physicsWorld.gravity = CGVector(dx: 0.0, dy: -15.0)
-            let box = SKSpriteNode(color: boxColor, size: CGSize(width: boxWidth, height: boxHeight))
-            //box.scale(to: <#T##CGSize#>)
-            box.position = location
-            box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: boxWidth, height: boxHeight))
-            addChild(box)
-        default:
-            boxColor = SKColor.cyan
-            boxWidth = 50
-            boxHeight = 50
-            let box = SKSpriteNode(color: boxColor, size: CGSize(width: boxWidth, height: boxHeight))
-            box.position = location
-            box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: boxWidth, height: boxHeight))
-            addChild(box)
+            case 1:
+                boxColor = SKColor.green
+                boxWidth = 40
+                boxHeight = 40
+                physicsWorld.gravity = CGVector(dx: 0.0, dy: -4.8)
+                let box = SKSpriteNode(color: boxColor, size: CGSize(width: boxWidth, height: boxHeight))
+                box.position = location
+                box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: boxWidth, height: boxHeight))
+                addChild(box)
+            case 2:
+                boxColor = SKColor.blue
+                boxWidth = 60
+                boxHeight = 60
+                physicsWorld.gravity = CGVector(dx: 0.0, dy: 0.5)
+                let texture1 = SKTexture(imageNamed: "a")
+                let arrow = SKSpriteNode(texture: texture1, color: UIColor.purple, size: CGSize(width: boxWidth, height: boxHeight))
+                arrow.position = location
+                arrow.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: boxWidth, height: boxHeight))
+                addChild(arrow)
+            case 3:
+                boxColor = SKColor.yellow
+                boxWidth = 30
+                boxHeight = 30
+                physicsWorld.gravity = CGVector(dx: 0.0, dy: -7.7)
+                let box = SKSpriteNode(color: boxColor, size: CGSize(width: boxWidth, height: boxHeight))
+                box.position = location
+                box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: boxWidth, height: boxHeight))
+                addChild(box)
+            case 4:
+                boxColor = SKColor.cyan
+                boxWidth = 44
+                boxHeight = 44
+                physicsWorld.gravity = CGVector(dx: 0.0, dy: -15.0)
+                let box = SKSpriteNode(color: boxColor, size: CGSize(width: boxWidth, height: boxHeight))
+                //box.scale(to: <#T##CGSize#>)
+                box.position = location
+                box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: boxWidth, height: boxHeight))
+                addChild(box)
+            case 5:
+                boxColor = SKColor.magenta
+                boxWidth = 50
+                boxHeight = 50
+                physicsWorld.gravity = CGVector(dx: 0.0, dy: -9.8)
+                let box = SKSpriteNode(color: boxColor, size: CGSize(width: boxWidth, height: boxHeight))
+                //box.scale(to: <#T##CGSize#>)
+                box.position = location
+                box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: boxWidth, height: boxHeight))
+                addChild(box)
+            default:
+                boxColor = SKColor.cyan
+                boxWidth = 50
+                boxHeight = 50
+                let box = SKSpriteNode(color: boxColor, size: CGSize(width: boxWidth, height: boxHeight))
+                box.position = location
+                box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: boxWidth, height: boxHeight))
+                addChild(box)
         }
-        
-        
     }
 }
 
