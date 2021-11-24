b69c95c8c912554c426e50c178065bff6def3bb8
Nicolas Ott <nicolasott@gmail.com>
2021-11-24 18:54:49 +0100

    0.1.1 Initial Commit

--- noGamX/ContentView.swift
+++ noGamX/ContentView.swift
@@ -1,88 +1,105 @@
-//
-//  ContentView.swift
-//  noGamX
-//
-//  Created by Nicolas Ott on 23.11.21.
-//
-
 import SwiftUI
 import CoreData
+import SpriteKit
 
-struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
 
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
-    var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
-                    }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
-        }
+// A simple game scene with falling boxes
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+        physicsWorld.gravity = CGVector(dx: 0.0, dy: -9.8)
+        let background = SKSpriteNode(imageNamed: "wallpaper")
+        background.position = CGPoint(x: size.width/2, y: size.height/2)
+        background.size = CGSize(width: CGFloat(2000), height: self.frame.size.height)
+        addChild(background)
     }
 
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+        
+        let randomInt = Int.random(in: 1..<5)
+        var boxColor = SKColor.red
+        var boxWidth = 50
+        var boxHeight = 50
+        switch randomInt {
+        case 1:
+            boxColor = SKColor.green
+            boxWidth = 40
+            boxHeight = 40
+            physicsWorld.gravity = CGVector(dx: 0.0, dy: -4.8)
+            let box = SKSpriteNode(color: boxColor, size: CGSize(width: boxWidth, height: boxHeight))
+            box.position = location
+            box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: boxWidth, height: boxHeight))
+            addChild(box)
+            
+        case 2:
+            boxColor = SKColor.blue
+            boxWidth = 60
+            boxHeight = 60
+            physicsWorld.gravity = CGVector(dx: 0.0, dy: 0.5)
+            let texture1 = SKTexture(imageNamed: "a")
+            let arrow = SKSpriteNode(texture: texture1, color: UIColor.purple, size: CGSize(width: boxWidth, height: boxHeight))
+            arrow.position = location
+            arrow.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: boxWidth, height: boxHeight))
+            addChild(arrow)
+            
+        case 3:
+            boxColor = SKColor.yellow
+            boxWidth = 30
+            boxHeight = 30
+            let box = SKSpriteNode(color: boxColor, size: CGSize(width: boxWidth, height: boxHeight))
+            box.position = location
+            box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: boxWidth, height: boxHeight))
+            addChild(box)
+            
+        case 4:
+            boxColor = SKColor.red
+            boxWidth = 44
+            boxHeight = 4
+            physicsWorld.gravity = CGVector(dx: 0.0, dy: -15.0)
+            let box = SKSpriteNode(color: boxColor, size: CGSize(width: boxWidth, height: boxHeight))
+            //box.scale(to: <#T##CGSize#>)
+            box.position = location
+            box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: boxWidth, height: boxHeight))
+            addChild(box)
+        default:
+            boxColor = SKColor.cyan
+            boxWidth = 50
+            boxHeight = 50
+            let box = SKSpriteNode(color: boxColor, size: CGSize(width: boxWidth, height: boxHeight))
+            box.position = location
+            box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: boxWidth, height: boxHeight))
+            addChild(box)
         }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
+        
+        
     }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
+// A sample SwiftUI creating a GameScene and sizing it
+// at 300x400 points
+struct ContentView: View {
+    
+    var scene: SKScene {
+        let scene = GameScene()
+        scene.size = CGSize(width: 300, height: 400)
+        scene.scaleMode = .resizeFill
+        return scene
+    }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+    var body: some View {
+        VStack {
+            GeometryReader { geo in
+                SpriteView(scene: scene)
+                    .frame(width: geo.size.width, height: geo.size.height)
+                    .ignoresSafeArea()
+            }
+           
+            Button("Clean", action: clean).edgesIgnoringSafeArea(.bottom)
+        }
+    }
+    func clean() {
+        
     }
 }
